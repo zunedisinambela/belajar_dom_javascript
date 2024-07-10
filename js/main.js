23e48c0f95ac009da416f6b802a14270e76f72b1
zunedisinambela <zunedisinambela@gmail.com>
2024-07-10 20:58:07 +0700

    query selector

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 document.title = 'Ubah Title'
 const btn1 = document.getElementById("btn1")
-const btn2 = document.getElementById("btn2")
+const btn2 = document.querySelector(".btn2")
 
 btn1.style.border = 'none'
 btn1.style.padding = '8px'
 btn1.style.fontSize = '24px'
 btn1.style.background = 'tomato'
+
+console.log(btn2)
